5f420459e8707ec3d445a41a88a7ed3849f5693e
AstridW <AstridWcountee@gmail.com>
2014-12-23 19:19:34 -0600

    deleted stuff and added crud functionality to views

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,30 +1,48 @@
 class PinsController < ApplicationController
+  before_action :find_pin, except: [:create, :new, :index]
 
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at desc")
   end
 
   def create
     @pin = Pin.new(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: "Your pin was successfully saved!"
+    else
+      render :new
+    end
   end
 
   def new
     @pin = Pin.new
   end
 
+  def show
+  end
+
+
   def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Your pin was sucessfully updated!"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @pin.destroy
+    redirect_to root_path, notice: "Your pin was sucessfully deleted"
   end
 
   private
 
-  def pins_params
+  def pin_params
     params.require(:pin).permit(:title,:description)
   end
 
-  def find_pins
+  def find_pin
+    @pin = Pin.find(params[:id])
   end
 
 end
